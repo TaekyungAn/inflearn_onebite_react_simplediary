35d1f8af16ec7355553cd0545c3c2c738f3263a0
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2023-01-20 16:47:13 +0900

    사용자 입력 처리하기(1)

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,77 +1,30 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
-const DiaryEditor = ({ onCreate }) => {
-  const authorInput = useRef();
-  const contentInput = useRef();
-
-  const [state, setState] = useState({
-    author: "",
-    content: "",
-    emotion: 1,
-  });
-
-  const handleChangeState = (e) => {
-    setState({
-      ...state,
-      [e.target.name]: e.target.value,
-    });
-  };
-  const handleSubmit = () => {
-    if (state.author.length < 1) {
-      authorInput.current.focus();
-      // alert("최소 1글자 이상 입력해주세요");
-      return;
-    }
-    if (state.content.length < 5) {
-      contentInput.current.focus();
-      // alert("일기 본문은 최소 5글자 이상 입력해주세요");
-      return;
-    }
-
-    onCreate(state.author, state.content, state.emotion);
-    alert("저장 성공!");
-    setState({
-      author: "",
-      content: "",
-      emotion: 1,
-    });
-  };
+const DiaryEditor = () => {
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
 
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
         <input
-          ref={authorInput}
-          name="author"
-          value={state.author}
-          onChange={handleChangeState}
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
         />
       </div>
       <div>
         <textarea
-          ref={contentInput}
-          name="content"
-          value={state.content}
-          onChange={handleChangeState}
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
         />
       </div>
-      <div>
-        <span>오늘의 감정 점수 : </span>
-        <select
-          name="emotion"
-          value={state.emotion}
-          onChange={handleChangeState}
-        >
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={4}>4</option>
-          <option value={5}>5</option>
-        </select>
-      </div>
-      <button onClick={handleSubmit}>일기 저장하기</button>
     </div>
   );
 };
-export default React.memo(DiaryEditor);
+
+export default DiaryEditor;
