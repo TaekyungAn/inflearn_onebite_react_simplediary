e328adb0a2e7d61f1298a572e5496898b94a66d2
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2023-01-23 22:41:26 +0900

    6-8 API호출하기

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import LifeCycle from "./LifeCylce";
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "이정환",
-//     content: "하이1",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-// ];
-
+// https://jsonplaceholder.typicode.com/comments
 const App = () => {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/comments`
+    ).then((res) => res.json());
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   // 새로운 일기를 추가하는 함수
   const onCreate = (author, content, emotion) => {
@@ -49,7 +60,6 @@ const App = () => {
 
   return (
     <div className="App">
-      <LifeCycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
